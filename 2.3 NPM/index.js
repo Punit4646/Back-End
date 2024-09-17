8eb2811827a045a1267a2db9321e9cf91529e8f7
Punit4646 <punitdalvadi84@gmail.com>
2024-09-17 18:42:51 +0530

    NPM

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,21 @@
+// var generateName = require('sillyname');
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+
+// import {randomSuperhero} from 'superheroes';
+
+// var randomSuperhero = require('superheroes');
+
+
+// console.log(`I am ${randomSuperhero}!`);
+
+import superheroes from "superheroes";
+
+const name = superheroes.random();
+
+console.log(`I am ${name}!`);
+
+
+
+
