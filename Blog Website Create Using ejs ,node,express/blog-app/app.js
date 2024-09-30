5cf2eb7c8368cd47b48c57b21a51bcee03df09c4
Punit4646 <punitdalvadi84@gmail.com>
2024-09-30 18:35:48 +0530

    Blog App Created Usnig Ejs,Node,Express

---
+++ Blog Website Create Using ejs ,node,express/blog-app/app.js
@@ -0,0 +1,31 @@
+// app.js
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+let posts = [];
+
+app.get('/', (req, res) => {
+  res.render('home', { posts: posts });
+});
+
+app.get('/compose', (req, res) => {
+  res.render('compose');
+});
+
+app.post('/compose', (req, res) => {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  };
+  posts.push(post);
+  res.redirect('/');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
